d2b8333dbc684706aca9ea6d7a3b0416df498163
Kirill Kulakov <kirill578@gmail.com>
2021-03-16 22:00:12 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box, Button } from '@material-ui/core'
+
+export const App = () => {
+  return (
+    <Box position="absolute" width="100%" height="100%" p="10px">
+      <Button variant="outlined">Button</Button>
+    </Box>
+  );
+}
