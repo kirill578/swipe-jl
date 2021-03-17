d972dc3398ed5d6d9615fe1f6cdb27413486a100
Kirill Kulakov <kirill578@gmail.com>
2021-03-17 13:51:45 -0500

    Updates

---
+++ precache-manifest.4465b70ac3a59c4aae84f60e319e500f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60dd27e68eb3c6a466ade189d4b19ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2ee5b83ff8a9a2bb275",
+    "url": "/static/css/main.50e0d94b.chunk.css"
+  },
+  {
+    "revision": "e33066ae6a7bee4d3332",
+    "url": "/static/js/2.6e28711d.chunk.js"
+  },
+  {
+    "revision": "f2ee5b83ff8a9a2bb275",
+    "url": "/static/js/main.a5987994.chunk.js"
+  },
+  {
+    "revision": "2e4bc1b3c022e2e6153b",
+    "url": "/static/js/runtime-main.d54b910c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/swipe-jl/precache-manifest.03183b1032a0bc65bca10914c03d1f10.js"
+  "/precache-manifest.4465b70ac3a59c4aae84f60e319e500f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/swipe-jl/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
