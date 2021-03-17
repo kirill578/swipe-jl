6db9aa2da44b278d70bca966f4e7a2796f92871b
Kirill Kulakov <kirill578@gmail.com>
2021-03-16 22:03:53 -0500

    Updates

---
+++ precache-manifest.accdbbda3ea8f7376ca92992c2190413.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbc72eee147a0af7a6695f598872652",
+    "url": "/swipe-jl/index.html"
+  },
+  {
+    "revision": "a3f8821000c2861e933d",
+    "url": "/swipe-jl/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bebf8db974faa59d1f66",
+    "url": "/swipe-jl/static/js/2.4a1b4e6d.chunk.js"
+  },
+  {
+    "revision": "a3f8821000c2861e933d",
+    "url": "/swipe-jl/static/js/main.636d76ce.chunk.js"
+  },
+  {
+    "revision": "e8f24dca83bb80c4c824",
+    "url": "/swipe-jl/static/js/runtime-main.de509387.js"
+  }
+]);
